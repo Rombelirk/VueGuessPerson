3aba675c2a3ab97f46c1ef5f4193e3a9ed4af8dc
User <user@iMac-User.local>
2019-02-25 12:47:59 +0300

    session

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -6,14 +6,12 @@ import sharedsession from "express-socket.io-session";
 import expressSession from "express-session";
 import express from "express"
 import path from "path"
-import cookieSession from "cookie-session"
 
-app.set('trust proxy', 1) // trust first proxy
-
-const session = cookieSession({
-    name: 'session',
-    keys: ['key1', 'key2']
-  });
+const session = expressSession({
+    secret: "my-secret",
+    resave: true,
+    saveUninitialized: true
+});
 
 app.use(express.static(__dirname+"/../../dist"));
 
